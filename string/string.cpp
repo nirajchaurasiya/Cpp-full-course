185db03b203b8111e1a68d85871d781389b10a74
Niraj Chaurasiya <developer.nirajchaurasiya@gmail.com>
2024-07-26 10:46:20 +0545

    string cpp

---
+++ string/string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string my_color;
+    cout << "Enter your fav. color: \n";
+    getline(cin, my_color);
+    cout << "Hey " << my_color << " is my fav. too" << endl;
+
+    return 0;
+}
