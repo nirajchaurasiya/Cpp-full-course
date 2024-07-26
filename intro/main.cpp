28abcb33800cf68847ad15eafe38327dfbb999eb
Niraj Chaurasiya <developer.nirajchaurasiya@gmail.com>
2024-07-26 10:00:17 +0545

    create main.cpp

---
+++ intro/main.cpp
@@ -0,0 +1,15 @@
+// #include <iostream>
+
+// int main()
+// {
+//     std::cout << "Hello There" << std::endl;
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello There" << endl;
+    return 0;
+}
